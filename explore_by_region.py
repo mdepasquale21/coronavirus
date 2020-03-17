9c1dc9866af7eb76f4a32a78fc87fa7fc1f86b53
Marco De Pasquale <depasquale.marco@gmail.com>
2020-03-17 10:35:48 +0100

    modified exploration by region

--- explore_by_region.py
+++ explore_by_region.py
@@ -19,15 +19,15 @@ print('\nINFO')
 dataset.info()
 print('\nDESCRIPTION')
 print(dataset.describe())
-n_rows_head = 10
-print('\nFIRST ' + str(n_rows_head) + ' ENTRIES')
-print(dataset.head(n_rows_head))
-print('\nMINIMUM VALUES')
-print(dataset.min())
-print('\nMAXIMUM VALUES')
-print(dataset.max())
-print('\nMEAN VALUES')
-print(dataset.mean())
+#n_rows_head = 10
+#print('\nFIRST ' + str(n_rows_head) + ' ENTRIES')
+#print(dataset.head(n_rows_head))
+#print('\nMINIMUM VALUES')
+#print(dataset.min())
+#print('\nMAXIMUM VALUES')
+#print(dataset.max())
+#print('\nMEAN VALUES')
+#print(dataset.mean())
 
 print('\n GENERAL DF')
 print(dataset.iloc[lambda x: x.index > 419][
@@ -43,7 +43,7 @@ print(dataset.iloc[lambda x: x.index > 419][
 # totale_attualmente_positivi,nuovi_attualmente_positivi,dimessi_guariti,deceduti,totale_casi,tamponi
 # ]
 ################################################################################################################################
-
+# monitoring hospitals in fvg
 last_date = '2020-03-16 17:00:00'
 last_tot_cases = dataset.loc[dataset['data']==last_date][['denominazione_regione','totale_casi','totale_attualmente_positivi', 'totale_ospedalizzati', 'terapia_intensiva']]
 
@@ -57,6 +57,8 @@ print('Ospedalizzati in terapia intensiva in fvg')
 print(last_tot_cases.loc[last_tot_cases['denominazione_regione']=='Friuli Venezia Giulia']['terapia_intensiva'].to_string(index=False))
 print('\n')
 
+################################################################################################################################
+# total cases by region
 print(last_tot_cases.to_string(index=False))
 
 counts_tot_cases_by_region = [
@@ -79,7 +81,7 @@ plt.clf()
 plt.close()
 
 ##############################################################################################################################
-# fvg
+# FVG
 days = [t[0] for t in enumerate(np.unique(dataset['data']))]
 data_fvg = dataset.loc[dataset['denominazione_regione']=='Friuli Venezia Giulia']
 
@@ -111,3 +113,36 @@ plt.plot(days, data_fvg['terapia_intensiva'], c='black', linestyle='-')
 plt.legend(('New Infected','In Hospital','Intensive Care'),loc='upper right', bbox_to_anchor=(1.05, 1.15), ncol=3)
 plt.savefig('./fvg-curves-3-hospital.png', dpi = 250)
 plt.clf()
+
+tot_fvg = dataset.loc[dataset['denominazione_regione']=='Friuli Venezia Giulia']['totale_casi']
+new_fvg = dataset.loc[dataset['denominazione_regione']=='Friuli Venezia Giulia']['nuovi_attualmente_positivi']
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+plt.ylabel('Total Cases')
+#plt.ylim(0,10)
+plt.xticks(rotation=90)
+plt.xlabel('Time (days after 24/02)')
+plt.title('Total Cases in FVG')
+plt.grid(linestyle='--', linewidth=0.2, color='lightgrey')
+for j,f in zip(days, tot_fvg):
+    plt.bar(j, f, color='red')
+plt.tight_layout()
+plt.savefig('fvg-total-cases.png', dpi=250)
+plt.clf()
+plt.close()
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+plt.ylabel('New Cases')
+#plt.ylim(0,10)
+plt.xticks(rotation=90)
+plt.xlabel('Time (days after 24/02)')
+plt.title('New Daily Cases in FVG')
+plt.grid(linestyle='--', linewidth=0.2, color='lightgrey')
+for j,f in zip(days, new_fvg):
+    plt.bar(j, f, color='orange')
+plt.tight_layout()
+plt.savefig('fvg-new-cases.png', dpi=250)
+plt.clf()
+plt.close()
