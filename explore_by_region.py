52314bb8a25344c928091085dc8af0649f021b64
Marco De Pasquale <depasquale.marco@gmail.com>
2020-03-17 09:48:36 +0100

    initial commit

---
+++ explore_by_region.py
@@ -0,0 +1,113 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import numpy as np
+
+matplotlib.rc('xtick', labelsize=5)
+matplotlib.rc('ytick', labelsize=5)
+
+# Importing the dataset
+dataset = pd.read_csv('./dati/dpc-covid19-ita-regioni.csv')
+
+################################################################################################################################
+
+print('\nDATA EXPLORATION')
+print('\nSHAPE')
+print(dataset.shape)
+print('\nINFO')
+dataset.info()
+print('\nDESCRIPTION')
+print(dataset.describe())
+n_rows_head = 10
+print('\nFIRST ' + str(n_rows_head) + ' ENTRIES')
+print(dataset.head(n_rows_head))
+print('\nMINIMUM VALUES')
+print(dataset.min())
+print('\nMAXIMUM VALUES')
+print(dataset.max())
+print('\nMEAN VALUES')
+print(dataset.mean())
+
+print('\n GENERAL DF')
+print(dataset.iloc[lambda x: x.index > 419][
+['data', 'denominazione_regione', 'totale_casi', 'totale_attualmente_positivi', 'nuovi_attualmente_positivi']
+].to_string())
+
+################################################################################################################################
+
+################################################################################################################################
+# features = [
+# data,stato,codice_regione,denominazione_regione,lat,long,
+# ricoverati_con_sintomi,terapia_intensiva,totale_ospedalizzati,isolamento_domiciliare,
+# totale_attualmente_positivi,nuovi_attualmente_positivi,dimessi_guariti,deceduti,totale_casi,tamponi
+# ]
+################################################################################################################################
+
+last_date = '2020-03-16 17:00:00'
+last_tot_cases = dataset.loc[dataset['data']==last_date][['denominazione_regione','totale_casi','totale_attualmente_positivi', 'totale_ospedalizzati', 'terapia_intensiva']]
+
+print('\nTotale casi in fvg')
+print(last_tot_cases.loc[last_tot_cases['denominazione_regione']=='Friuli Venezia Giulia']['totale_casi'].to_string(index=False))
+print('Totale attualmente infetti in fvg')
+print(last_tot_cases.loc[last_tot_cases['denominazione_regione']=='Friuli Venezia Giulia']['totale_attualmente_positivi'].to_string(index=False))
+print('Totale ospedalizzati in fvg')
+print(last_tot_cases.loc[last_tot_cases['denominazione_regione']=='Friuli Venezia Giulia']['totale_ospedalizzati'].to_string(index=False))
+print('Ospedalizzati in terapia intensiva in fvg')
+print(last_tot_cases.loc[last_tot_cases['denominazione_regione']=='Friuli Venezia Giulia']['terapia_intensiva'].to_string(index=False))
+print('\n')
+
+print(last_tot_cases.to_string(index=False))
+
+counts_tot_cases_by_region = [
+(region, last_tot_cases.loc[last_tot_cases['denominazione_regione']==region]['totale_casi']) for region in np.unique(dataset['denominazione_regione'])
+]
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+plt.ylabel('Total Cases')
+#plt.ylim(0,10)
+plt.xticks(rotation=90)
+plt.xlabel('Region')
+plt.title('Total Cases by region')
+plt.grid(linestyle='--', linewidth=0.2, color='lightgrey')
+for j,f in counts_tot_cases_by_region:
+    plt.bar(j, f)
+plt.tight_layout()
+plt.savefig('total_cases_by_region.png', dpi=250)
+plt.clf()
+plt.close()
+
+##############################################################################################################################
+# fvg
+days = [t[0] for t in enumerate(np.unique(dataset['data']))]
+data_fvg = dataset.loc[dataset['denominazione_regione']=='Friuli Venezia Giulia']
+
+plt.xlabel('Time (days after 24/02)')
+plt.ylabel('Values for FVG')
+plt.plot(days, data_fvg['totale_casi'], c='red', linestyle='-')
+plt.plot(days, data_fvg['totale_attualmente_positivi'], c='pink', linestyle='-')
+plt.plot(days, data_fvg['nuovi_attualmente_positivi'], c='orange', linestyle='-')
+plt.plot(days, data_fvg['dimessi_guariti'], c='green', linestyle='-')
+plt.plot(days, data_fvg['deceduti'], c='blueviolet', linestyle='-')
+plt.legend(('Total Cases','Currently Infected','New Infected','Healed','Deceased'),loc='upper right', bbox_to_anchor=(1.05, 1.15), ncol=3)
+plt.savefig('./fvg-curves.png', dpi = 250)
+plt.clf()
+
+plt.xlabel('Time (days after 24/02)')
+plt.ylabel('Values for FVG')
+plt.plot(days, data_fvg['nuovi_attualmente_positivi'], c='orange', linestyle='-')
+plt.plot(days, data_fvg['dimessi_guariti'], c='green', linestyle='-')
+plt.plot(days, data_fvg['deceduti'], c='blueviolet', linestyle='-')
+plt.legend(('New Infected','Healed','Deceased'),loc='upper right', bbox_to_anchor=(1.05, 1.15), ncol=3)
+plt.savefig('./fvg-curves-2.png', dpi = 250)
+plt.clf()
+
+plt.xlabel('Time (days after 24/02)')
+plt.ylabel('Values for FVG')
+plt.plot(days, data_fvg['nuovi_attualmente_positivi'], c='orange', linestyle='-')
+plt.plot(days, data_fvg['totale_ospedalizzati'], c='grey', linestyle='-')
+plt.plot(days, data_fvg['terapia_intensiva'], c='black', linestyle='-')
+plt.legend(('New Infected','In Hospital','Intensive Care'),loc='upper right', bbox_to_anchor=(1.05, 1.15), ncol=3)
+plt.savefig('./fvg-curves-3-hospital.png', dpi = 250)
+plt.clf()
---
+++ plotscatter.py
@@ -0,0 +1,226 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+import pandas as pd
+import seaborn as sns
+
+#import data
+dataset = pd.read_csv('./dati/dpc-covid19-ita-andamento-nazionale.csv')
+
+print('\nDATA EXPLORATION')
+print('\nSHAPE')
+print(dataset.shape)
+print('\nINFO')
+dataset.info()
+print('\nDESCRIPTION')
+print(dataset.describe())
+n_rows_head = 18
+print('\nFIRST ' + str(n_rows_head) + ' ENTRIES')
+print(dataset.head(n_rows_head))
+print('\nMINIMUM VALUES')
+print(dataset.min())
+print('\nMAXIMUM VALUES')
+print(dataset.max())
+print('\nMEAN VALUES')
+print(dataset.mean())
+
+print('\n GENERAL DF')
+print(dataset[
+['data', 'totale_casi', 'totale_attualmente_positivi', 'nuovi_attualmente_positivi']
+].to_string())
+
+################################################################################################################################
+# features = [
+# data, stato, ricoverati_con_sintomi, terapia_intensiva, totale_ospedalizzati, isolamento_domiciliare,
+# totale_attualmente_positivi, nuovi_attualmente_positivi,
+# dimessi_guariti, deceduti, totale_casi, tamponi
+# ]
+################################################################################################################################
+
+days = [t[0] for t in enumerate(dataset['data'])]
+
+# create scatter plot
+plt.xlabel('Time (days after 24/02)')
+plt.ylabel('Total Cases')
+plt.title('Epidemic curve')
+plt.scatter(days, dataset['totale_casi'], c='red')
+#plt.scatter(days, dataset['totale_casi'], c='black')
+plt.legend(('data',),loc='upper right', bbox_to_anchor=(1.05, 1.15))
+plt.savefig('./curves/epidemic-curve.png', dpi = 250)
+plt.clf()
+
+plt.xlabel('Time (days after 24/02)')
+plt.ylabel('Log of Total Cases')
+plt.title('Epidemic curve')
+plt.scatter(days, np.log(dataset['totale_casi']), c='red')
+plt.legend(('data',),loc='upper right', bbox_to_anchor=(1.05, 1.15))
+plt.savefig('./curves/epidemic-curve-log.png', dpi = 250)
+plt.clf()
+
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
+
+x = [[days[i]] for i in range(len(days))] #make a 2-D array
+#print(x)
+y= np.log(dataset['totale_casi'])
+
+#Building the model
+regressor = LinearRegression()
+#regressor = Ridge()
+#regressor = Lasso()
+
+regressor.fit(x,y)
+Y_pred_train = regressor.predict(x) # useful for plot
+
+score = regressor.score(x, y) # R2 score
+growth = regressor.coef_[0] # coefficients reg.coef_ is an array in general, when fitting multidimensional X values
+n0 = regressor.intercept_ # intercepts
+
+print('\nSCORE:\n', score)
+#print('Y TRUE - Y PREDICTED DIFFERENCES:\n', y - Y_pred_train) # difference between true and predicted values
+print('COEFFICIENT:\n', growth)
+print('INTERCEPT:\n', n0)
+
+#Training
+plt.scatter(x,y,color = 'red')
+plt.plot(x,Y_pred_train,color = 'blue')
+plt.title('Epidemic curve Fit')
+plt.xlabel('Time (days after 24/02)')
+plt.ylabel('Log of Total Cases')
+plt.legend(('fit (R2={:.3f})'.format(score), 'data'),loc='upper right', bbox_to_anchor=(1.05, 1.15))
+plt.savefig('./curves/epidemic-curve-fit-TRAIN.png', dpi=250)
+plt.clf()
+
+last = len(dataset['totale_casi'])
+print('\nPREDICTION FOR DAY',last)
+pred = regressor.predict([[last]])
+print(pred)
+print('number of predicted infected', np.exp(pred))
+
+################################################################################################################################
+
+# new infected
+plt.xlabel('Time (days after 24/02)')
+plt.ylabel('New Infected')
+plt.title('New infected curve')
+plt.scatter(days, dataset['nuovi_attualmente_positivi'], c='orange')
+plt.legend(('data',),loc='upper right', bbox_to_anchor=(1.05, 1.15))
+plt.savefig('./curves/new-infected-curve.png', dpi = 250)
+plt.clf()
+
+# healed people
+plt.xlabel('Time (days after 24/02)')
+plt.ylabel('Healed')
+plt.title('Healed curve')
+plt.scatter(days, dataset['dimessi_guariti'], c='green')
+plt.legend(('data',),loc='upper right', bbox_to_anchor=(1.05, 1.15))
+plt.savefig('./curves/healed-curve.png', dpi = 250)
+plt.clf()
+
+# deceased people
+plt.xlabel('Time (days after 24/02)')
+plt.ylabel('Deceased')
+plt.title('Deceased curve')
+plt.scatter(days, dataset['deceduti'], c='blueviolet')
+plt.legend(('data',),loc='upper right', bbox_to_anchor=(1.05, 1.15))
+plt.savefig('./curves/deceased-curve.png', dpi = 250)
+plt.clf()
+
+################################################################################################################################
+
+#everything
+plt.xlabel('Time (days after 24/02)')
+plt.ylabel('Values')
+#plt.title('All curves')
+plt.plot(days, dataset['totale_casi'], c='red', linestyle='-')
+plt.plot(days, dataset['nuovi_attualmente_positivi'], c='orange', linestyle='-')
+plt.plot(days, dataset['dimessi_guariti'], c='green', linestyle='-')
+plt.plot(days, dataset['deceduti'], c='blueviolet', linestyle='-')
+plt.legend(('Total Cases','New Infected','Healed','Deceased'),loc='upper right', bbox_to_anchor=(1.05, 1.15), ncol=2)
+plt.savefig('./curves/all-curves.png', dpi = 250)
+plt.clf()
+
+# new positives, healed, deceased
+plt.xlabel('Time (days after 24/02)')
+plt.ylabel('Values')
+plt.plot(days, dataset['nuovi_attualmente_positivi'], c='orange', linestyle='-')
+plt.plot(days, dataset['dimessi_guariti'], c='green', linestyle='-')
+plt.plot(days, dataset['deceduti'], c='blueviolet', linestyle='-')
+plt.legend(('New Infected','Healed','Deceased'),loc='upper right', bbox_to_anchor=(1.05, 1.15), ncol=3)
+plt.savefig('./curves/almost-all-curves.png', dpi = 250)
+plt.clf()
+
+print('\ntotale_casi = totale_attualmente_positivi + dimessi_guariti + deceduti\n')
+
+#total cases and total currently positive
+plt.xlabel('Time (days after 24/02)')
+plt.ylabel('Values')
+plt.plot(days, dataset['totale_casi'], c='red', linestyle='-')
+plt.plot(days, dataset['totale_attualmente_positivi'], c='pink', linestyle='-')
+plt.plot(days, dataset['nuovi_attualmente_positivi'], c='orange', linestyle='-')
+plt.plot(days, dataset['dimessi_guariti'], c='green', linestyle='-')
+plt.plot(days, dataset['deceduti'], c='blueviolet', linestyle='-')
+plt.legend(('Total Cases','Currently Infected','New Infected','Healed','Deceased'),loc='upper right', bbox_to_anchor=(1.05, 1.15), ncol=3)
+plt.savefig('./curves/all-case-all-current-curves.png', dpi = 250)
+plt.clf()
+
+################################################################################################################################
+
+# fatality rate
+fatality_rate_1 = dataset['deceduti']/dataset['totale_casi']
+fatality_rate_2 = dataset['deceduti']/(dataset['deceduti'] + dataset['dimessi_guariti'])
+plt.xlabel('Time (days after 24/02)')
+plt.ylabel('Fatality Rate')
+plt.plot(days, fatality_rate_1, c='darkslateblue', linestyle='-')
+plt.plot(days, fatality_rate_2, c='saddlebrown', linestyle='-')
+plt.legend(('dec/tot.cases','dec/(dec+heal)'),loc='upper right', bbox_to_anchor=(1.05, 1.15), ncol=2)
+plt.savefig('./curves/fatality-rate.png', dpi = 250)
+plt.clf()
+
+# calculate growth factor
+growth_factor = []
+
+for i, _ in enumerate(dataset['totale_casi']):
+    if(i < 2):
+        print('\nCalculating growth factors: skipping index',i)
+    else:
+        n2 = dataset['totale_casi'].iloc[i]
+        n1 = dataset['totale_casi'].iloc[i-1]
+        n0 = dataset['totale_casi'].iloc[i-2]
+        growth_factor.append((n2-n1)/(n1-n0))
+
+print('\ngrowth factors day by day')
+print(growth_factor)
+
+# growth factor
+plt.xlabel('Time (days after 24/02)')
+plt.ylabel('Growth Factor')
+plt.title('Growth Factor [dN(t+1)/dN(t)]')
+plt.plot(days[2:], growth_factor, 'ko')
+plt.plot(days[2:], growth_factor, color='grey', linestyle='--')
+#plt.legend(('data','linked'),loc='upper right', bbox_to_anchor=(1.05, 1.15), ncol=2)
+plt.savefig('./curves/growth-factor.png', dpi = 250)
+plt.clf()
+
+# calculate growth rate
+growth_rate = []
+
+for i, _ in enumerate(dataset['totale_casi']):
+    if(i < 1):
+        print('\nCalculating growth rates: skipping index',i)
+    else:
+        n2 = dataset['totale_casi'].iloc[i]
+        n1 = dataset['totale_casi'].iloc[i-1]
+        growth_rate.append(((n2-n1)/n1)*100.0)
+
+print('\ngrowth rates % day by day')
+print(growth_rate)
+
+# growth rate
+plt.xlabel('Time (days after 24/02)')
+plt.ylabel('Growth Rate %')
+plt.title('Growth Rate [(N(t+1)-N(t))/N(t)]x100')
+plt.plot(days[1:], growth_rate, 'bo')
+plt.plot(days[1:], growth_rate, color='cyan', linestyle='--')
+#plt.legend(('data','linked'),loc='upper right', bbox_to_anchor=(1.05, 1.15), ncol=2)
+plt.savefig('./curves/growth-rate.png', dpi = 250)
+plt.clf()
