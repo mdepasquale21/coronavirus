71edbe356b25f76fda0cfd68a7ade11065209a3e
Marco De Pasquale <depasquale.marco@gmail.com>
2020-03-30 19:00:14 +0200

    update to 30th of march

--- trieste.py
+++ trieste.py
@@ -42,7 +42,7 @@ dataset.info()
 # monitoring Trieste
 print('\n MONITORING SITUATION IN TRIESTE')
 
-last_date = '2020-03-29T17:00:00'
+last_date = '2020-03-30T17:00:00'
 last_tot_cases = dataset.loc[dataset['data']==last_date][['denominazione_provincia','totale_casi']]
 
 casi_trieste = last_tot_cases.loc[last_tot_cases['denominazione_provincia']=='Trieste']['totale_casi']
