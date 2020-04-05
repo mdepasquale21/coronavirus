5d1c515638b8d4697fe12a88f0506368f404ab4b
Marco De Pasquale <depasquale.marco@gmail.com>
2020-04-05 22:04:14 +0200

    corrected new analysis

--- new_fit_2.py
+++ new_fit_2.py
@@ -22,12 +22,10 @@ regressor2 = LinearRegression()
 regressor3 = LinearRegression()
 regressor4 = LinearRegression()
 regressor5 = LinearRegression()
-regressor6 = LinearRegression()
-regressor7 = LinearRegression()
 
-regressors = (regressor1,regressor2,regressor3,regressor4,regressor5,regressor6,regressor7)
+regressors = (regressor1,regressor2,regressor3,regressor4,regressor5)
 
-map = {1:30, 2:25, 3:20, 4:15, 5:10, 6:5, 7:None}
+map = {1:-28, 2:-21, 3:-14, 4:-7, 5:None}
 
 log_y_pred = []
 scores = []
@@ -38,11 +36,11 @@ for reg, i in zip(regressors, range(1, len(regressors)+1)):
     yy = log_y[:map[i]]
     reg.fit(xx,yy)
     log_y_pred.append(reg.predict(x))
-    scores.append(reg.score(x, log_y))  # R2 score
+    scores.append(reg.score(xx, yy))  # R2 score
     growths.append(reg.coef_[0]) # coefficients reg.coef_ is an array in general, when fitting multidimensional X values
     n0s.append(reg.intercept_)  # intercepts
 
-colors = ('orange', 'green', 'yellow', 'purple', 'brown', 'grey', 'blue')
+colors = ('grey', 'green', 'purple', 'brown', 'blue')
 
 #plot of fit vs data
 plt.scatter(x,log_y,color = 'red')
@@ -51,7 +49,14 @@ for i in range(len(regressors)):
 #plt.title('Epidemic curve vs Fit')
 plt.xlabel('Time (days after 24/02)')
 plt.ylabel('Log of Total Cases')
-#plt.legend((['fit (R2={:.3f})'.format(score) for score in scores], 'log data'),loc='upper right', bbox_to_anchor=(1.05, 1.15), ncol=4)
+plt.legend((
+'fit (R2={:.3f})'.format(scores[0]),
+'fit (R2={:.3f})'.format(scores[1]),
+'fit (R2={:.3f})'.format(scores[2]),
+'fit (R2={:.3f})'.format(scores[3]),
+'fit (R2={:.3f})'.format(scores[4]),
+'log data'),
+loc='upper right', bbox_to_anchor=(1.05, 1.15), ncol=3)
 plt.grid(color='lightgray', linestyle='--', linewidth=0.5)
 plt.savefig('./FIT/fit_1_time.png', dpi=250)
 plt.tight_layout()
@@ -64,7 +69,14 @@ for i in range(len(regressors)):
 #plt.title('Epidemic curve vs Fit')
 plt.xlabel('Time (days after 24/02)')
 plt.ylabel('Total Cases')
-#plt.legend((['fit (R2={:.3f})'.format(score) for score in scores], 'data'),loc='upper right', bbox_to_anchor=(1.05, 1.15), ncol=4)
+plt.legend((
+'fit (R2={:.3f})'.format(scores[0]),
+'fit (R2={:.3f})'.format(scores[1]),
+'fit (R2={:.3f})'.format(scores[2]),
+'fit (R2={:.3f})'.format(scores[3]),
+'fit (R2={:.3f})'.format(scores[4]),
+'data'),
+loc='upper right', bbox_to_anchor=(1.05, 1.15), ncol=3)
 plt.grid(color='lightgray', linestyle='--', linewidth=0.5)
 plt.tight_layout()
 plt.savefig('./FIT/fit_2_time.png', dpi=250)
