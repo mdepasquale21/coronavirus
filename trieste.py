10bac3298cf54a6bd2904b7cf8cab3fc788ee743
Marco De Pasquale <depasquale.marco@gmail.com>
2020-05-07 18:33:50 +0200

    update to 7th of may

--- trieste.py
+++ trieste.py
@@ -35,11 +35,11 @@ dataset.info()
 ################################################################################################################################
 
 # yesterday
-yesterday='2020-05-05T17:00:00'
+yesterday='2020-05-06T17:00:00'
 yesterday_tot_cases = dataset.loc[dataset['data']==yesterday][['denominazione_provincia','totale_casi']]
 
 # today
-last_date = '2020-05-06T17:00:00'
+last_date = '2020-05-07T17:00:00'
 last_tot_cases = dataset.loc[dataset['data']==last_date][['denominazione_provincia','totale_casi']]
 
 # trieste
